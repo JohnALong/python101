2b3cc6151ab1ace644bad384cff8be11a065c395
John Long <jlcomp45@gmail.com>
2020-01-13 16:25:09 -0600

    completed dictionary exercises

---
+++ dictionaries/dictionaryOfWords.py
@@ -0,0 +1,24 @@
+"""
+Create a dictionary with key value pairs to
+represent words (key) and its definition (value)
+"""
+word_definitions = dict()
+
+"""
+Add several more words and their definitions
+   Example: word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+"""
+
+word_definitions["cattywampus"] = "askew, awry, kitty-corner"
+word_definitions["bumfuzzle"] = "confuse; perplex; fluster"
+word_definitions["Gardyloo"] = "used in Edinburgh as a warning cry when it was customary to throw slops from the windows into the streets"
+word_definitions["Taradiddle"] = "1: a fib 2 : pretentious nonsense"
+word_definitions["Snickersnee"] = "to engage in cut-and-thrust fighting with knives"
+
+# print single definition
+# print(word_definitions["Snickersnee"])
+# print(word_definitions["Gardyloo"])
+
+# print all definitions in word_definitions
+for (key, value) in word_definitions.items():
+    print(f'The definition of {key} is {value}')
