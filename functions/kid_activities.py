eefe4e1c60d9082002b8c6d366843a973acd2ea4
John Long <jlcomp45@gmail.com>
2020-01-14 20:45:22 -0600

    completed lists challenge

--- functions/kid_activities.py
+++ functions/kid_activities.py
@@ -19,42 +19,42 @@
 #         "first_name": first_name,
 #         "last_name": last_name,
 #         "age": age,
-#         "occupation": occupation,
+#         "occupation": occupation, 
 #     }
 
 # melissa = create_person("Melissa", "Bell", 25, "Software Developer")
 # print(melissa)
 # function for run
-# running_kids = ["Pam", "Sam", "Andrea", "Will"]
-# def run(name):
-#     print(f"{child} ran desperately towards NSS")
+running_kids = ["Pam", "Sam", "Andrea", "Will"]
+def run(name):
+    print(f"{child} ran desperately towards NSS")
 
-# for child in running_kids:
-#     run(child)
+for child in running_kids:
+    run(child)
 
 # # function for swinging
-# swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
-# def swing(name):
-#     print(f"{child} swung over the canyon as if they had no fear!")
+swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+def swing(name):
+    print(f"{child} swung over the canyon as if they had no fear!")
 
-# for child in running_kids:
-#     swing(child)
+for child in running_kids:
+    swing(child)
 
 # #function for sliding
-# sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
-# def slide(name):
-#     print(f"{name} slid into capstone by the skin of their teeth")
+sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+def slide(name):
+    print(f"{name} slid into capstone by the skin of their teeth")
 
-# for name in sliding_kids:
-#     slide(name)
+for name in sliding_kids:
+    slide(name)
 
 # #function for hiding
-# hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
-# def hide(name):
-#     print(f"{name} hid their deficiencies by studying longer than everyone else")
+hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
+def hide(name):
+    print(f"{name} hid their deficiencies by studying longer than everyone else")
 
-# for name in hiding_kids:
-#     hide(name)
+for name in hiding_kids:
+    hide(name)
 
 #Chicken Monkey
 # generate a list of numbers 1..100
--- lists/planets.py
+++ lists/planets.py
@@ -13,3 +13,19 @@ rocky_planets = planet_list[0:4]
 print(rocky_planets)
 del planet_list[-1]
 print(planet_list)
+
+# spacecraft list
+spacecraft = [
+    ("Rover", "Mercury"),
+    ("Explorer", "Neptune"),
+    ("Lost", "Venus"),
+    ("Found", "Mars"),
+    ("Why", "Uranus"),
+    ("Because", "Jupiter"),
+    ("The Rings of", "Saturn"),
+]
+for planet in planet_list:
+    for craft in spacecraft:
+        if planet == craft[1]:
+            print(f"{craft[0]} has visited {planet}")
+        
