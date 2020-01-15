f57c75b703f614eed80c319099c4c13842324c3d
John Long <jlcomp45@gmail.com>
2020-01-15 10:08:13 -0600

    working on dictionary challenge

---
+++ dictionaries/family_dictionary.py
@@ -0,0 +1,27 @@
+my_family = {
+    "sister": {
+        "name": "Brenda",
+        "age": 52
+    },
+    "mother": {
+        "name": "Deanna",
+        "age": 75
+    },
+    "father": {
+        "name": "Jim",
+        "age": 75
+    },
+    "spouse": {
+        "name": "Jeanne",
+        "age": 44
+    }
+}
+
+
+
+print(my_family["sister"].get("name"))
+# family_strings = []
+# family_strings = [ f"My {key}'s name is {value}" for key, value in my_family.items() ]
+# print(family_strings)
+
+ 
--- functions/cashToCoins.py
+++ functions/cashToCoins.py
@@ -1,3 +1,5 @@
+import math
+
 def calc_coins():
 # starting cash total
     dollarAmount = 8.69
@@ -27,4 +29,57 @@ def calc_coins():
     print(piggyBank)
 
 calc_coins()
+
+# Joe's version of exercise - imported math
+def calc_coins_joe():
+# starting cash total
+    dollarAmount = 8.69
+
+
+    piggyBank = {
+    "pennies": 0,
+    "nickels": 0,
+    "dimes": 0,
+    "quarters": 0
+    }
+    
+    piggyBank["quarters"] = math.floor(dollarAmount / .25)
+    dollarAmount = dollarAmount % .25
+    piggyBank["dimes"] = math.floor(dollarAmount / .10)
+    dollarAmount = dollarAmount % .10
+    piggyBank["nickels"] = math.floor(dollarAmount / .05)
+    dollarAmount = dollarAmount % .05
+    piggyBank["pennies"] = math.ceil(dollarAmount / .01)
+    dollarAmount = dollarAmount % .01
+
+    return piggyBank
+
+print(calc_coins_joe())
+
+# Sophia's solution
+# def fill_piggy_bank (dollarAmount):
+#     quarters_quantity = 0
+#     dimes_quantity = 0
+#     nickels_quantity = 0
+#     pennies_quantity = 0
+#     while dollarAmount > .25:
+#         dollarAmount -= .25
+#         quarters_quantity += 1
+#     while dollarAmount > .1:
+#         dollarAmount -= .1
+#         dimes_quantity += 1
+#     while dollarAmount > .05:
+#         dollarAmount -= .05
+#         nickels_quantity += 1
+#     while dollarAmount > .01:
+#         dollarAmount -= .01
+#         pennies_quantity += 1
+#     piggyBank.update({
+#         'quarters': quarters_quantity,
+#         'dimes': dimes_quantity,
+#         'nickels': nickels_quantity,
+#         'pennies': pennies_quantity
+#     })
+#     print(piggyBank)
+# fill_piggy_bank(dollarAmount)
     
